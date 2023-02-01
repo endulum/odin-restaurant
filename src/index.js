43ca6be8bc7882d6f4aa9c779b94ff931f86e82f
endulum <apheum@zoho.com>
2023-02-01 15:03:38 -0800

    basic styling, made navbar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
 import './style.css';
-
-document.write('a journey of a thousand miles');
-console.warn('uwu');
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  }
 };
