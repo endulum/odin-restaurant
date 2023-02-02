578c0ad5ce2f73dc52438853fa3183299dc3839b
endulum <apheum@zoho.com>
2023-02-02 13:00:04 -0800

    building mainpage content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import Icecream from './icecream.png';
+
+const $ = selector => document.querySelector(selector);
+
+const ic = new Image();
+ic.src = Icecream;
+
+$('.circle').appendChild(ic);
--- webpack.config.js
+++ webpack.config.js
@@ -29,5 +29,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
