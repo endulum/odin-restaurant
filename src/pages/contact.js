7e2a24be99acd0015b36845c867cf1fd53e43352
endulum <apheum@zoho.com>
2023-02-06 15:34:57 -0800

    finished contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,40 @@ export default function loadContact() {
     console.warn('loadContact invoked.');
     empty();
 
+    let inquiry = document.createElement('form');
+    inquiry.classList.add('inquiry');
+    content.appendChild(inquiry);
+
+    let inquiryText = document.createElement('div');
+    inquiryText.classList.add('text');
+    inquiryText.innerHTML = `<h2>HAVE AN INQUIRY?</h2><p>Whatever you got, the team is always willing to listen. Your voices are valuable to us.</p><p>You may also directly contact an individual of our team using the information below.</p>`;
+
+    inquiry.appendChild(inquiryText);
+
+    let inquiryDetails = document.createElement('div');
+    inquiryDetails.classList.add('details');
+    inquiryDetails.innerHTML = `
+    <div>
+        <label for="name">Your Name</label>
+        <input type="text" id="name" name="name" required>
+    </div><div>
+        <label for="email">E-Mail</label>
+        <input type="email" id="email" name="email" required>
+    </div><div>
+        <label for="tel">Phone #</label>
+        <input type="tel" id="tel" name="tel" required>
+    </div>`;
+
+    inquiry.appendChild(inquiryDetails);
+
+    let inquirySubmit = document.createElement('button');
+    inquirySubmit.textContent = 'Submit';
+    inquiry.appendChild(inquirySubmit);
+
+    let inquiryBox = document.createElement('textarea');
+    inquiryBox.setAttribute('placeholder', 'Your concerns, praise, or otherwise go here.')
+    inquiry.appendChild(inquiryBox);
+
     let people = document.createElement('div');
     people.classList.add('people');
     content.appendChild(people);
